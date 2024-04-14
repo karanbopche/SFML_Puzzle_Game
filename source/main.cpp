81766b255293587cde726d274656abf756107a90
8knypx <karan.bopche@aptiv.com>
2024-04-15 00:48:04 +0530

    Add Gtest Support

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,27 @@
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
-int main(){
-    sf::Window window(sf::VideoMode(800, 600), "Cards Game");
-
-    while(window.isOpen()){
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Cards Game");
+    sf::Texture background;
+    if (!background.loadFromFile("assets/images/puzzle_Image_1.jpg"))
+    {
+        return -1;
+    }
+    sf::Sprite bgSprite(background);
+    while (window.isOpen())
+    {
         sf::Event event;
-        while(window.pollEvent(event)){
+        while (window.pollEvent(event))
+        {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+        window.clear();
+        window.draw(bgSprite);
+        window.display();
+        sf::sleep(sf::milliseconds(100));
     }
 
     return 0;
---
+++ tests/sanity_test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(sanity_check, basic_test)
+{
+  // Expect equality.
+  EXPECT_TRUE(true);
+}
